b7983a83cf6d93229a180b75a7bb2a7862e40e96
yihutu <gongjinmin@outlook.com>
2018-09-20 13:50:36 +0800

    输入数字显示图像

---
+++ week1/day1/输入图形2.py
@@ -0,0 +1,11 @@
+#conding:utf-8
+hig = int(input("hig:"))
+wid = int(input("wid:"))
+num2 = hig
+while num2>0:
+    num1 = hig
+    while num1>0:
+        print("#",end="")
+        num1 -=1
+    print()
+    num2 -=1
